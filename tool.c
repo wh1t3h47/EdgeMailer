7dd5cf82b1538e5c7424186d44a05dceeb2babed
wh1t3h47 <tom.mharres@gmail.com>
2020-08-11 14:06:03 -0300

    Add malloc/ free

--- tool.c
+++ tool.c
@@ -8,7 +8,6 @@
 
 char *proxy = NULL; // CURLOPT_PROXY
 void err_exit();
-bool is_allocated = false; // if true, we need to free socket_controller
 
 typedef struct socket_controller {
 	/* TODO poll_handle description
@@ -19,6 +18,20 @@ typedef struct socket_controller {
 	curl_socket_t socket_fd;
 } socket_controller_t;
 
+socket_controller_t *create_controller() {
+	socket_controller_t *controller;
+	controller = malloc( sizeof(*controller) );
+	if ( controller == NULL ) { // malloc returns NULL on fail or when malloc(0)
+		err_exit("Malloc failed: Out of memory");
+	}
+	return controller;
+}
+
+void free_controller(void *arg) { // we cast to void ptr because this is *ALSO* a callback
+	socket_controller_t *controller = (socket_controller_t *) arg;
+	free(controller);
+}
+
 int main( int argc, char *argv[] ) {
 	if ( argc > 0 ) {
 		if ( argc == 1 ) { // First optional arg is proxy
@@ -26,18 +39,21 @@ int main( int argc, char *argv[] ) {
 		}
 		else {
 			errno = E2BIG; // Argument list too long
-			err_exit();
+			err_exit(NULL);
 		}
 	}
 }
 
-void err_exit() {
-	if ( errno != 0 ) {
+void err_exit(char *message) {
+	if ( message != NULL ) { // if caller specified custom message
+		printf("%s\n", message);
+	}
+	else if ( errno != 0 ) { // we don't wanne print "Success"
 		char *msg;
 		msg = strerror(errno); // fetch error message
 		printf("%s\n", msg);
 	}
 	// FIXME Libuv clean up
 	// FIXME Libcurl clean up
-	exit(-1);
+	exit(-1); // exit frees memory automagically
 }
